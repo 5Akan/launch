a1d507abac592c53797ab6c5646a5054740b7df0
5Akan <akanimohsobot@gmail.com>
2023-07-21 18:13:36 +0100

    Fetching API in setup with Vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,50 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>HOME</h1>
+<div v-if="err">Problem With Page</div>
+<div v-else-if= "posts.length">
+  <PostList v-if = "showPost" :posts = "posts"/>
+</div>
+<div v-else>{{loading}}</div>
+    
+    <button @click= "showPost = !showPost">Show Post</button>
+    <button @click= "posts.pop()">Remove Post</button>
+    
   </div>
 </template>
-
 <script>
+import PostList from '../components/PostList.vue';
+import { ref } from 'vue';
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
+  components:{PostList},
+  setup(){
+    const posts = ref([]);
+    const err = ref();
+    const loading = ref("Loading Page");
+   const url = async()=>{
+     try{
+        let data = await fetch('http://localhost:3000/post');
+       if(!data.ok){
+         throw Error("Url problem");
+       }else{
+        posts.value = await data.json();  
+       }
+       
+
+     }catch(error){
+       err.value = error.message;
+     }
+   }
+  url()
+   const showPost = ref(true);
+
+   return{showPost, posts,err,loading}
+  
   }
 }
+
 </script>
